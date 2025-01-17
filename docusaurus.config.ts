de9662ab63ab6e8f81044e474f95bfb4ae3c1940
2nan222 <dame2623@datacrush.ai>
2025-01-17 15:31:22 +0900

    test: 배포 연결 확인 테스트

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: '2nan',
+  tagline: '수리중입니다 조금만 기다려주세요',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
